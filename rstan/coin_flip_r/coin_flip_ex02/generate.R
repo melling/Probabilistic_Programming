6420bafd9e87312e72af516939db024bb6905bcb
Michael Mellinger <mmellinger66@gmail.com>
2020-11-19 14:19:33 -0500

    Optimizing line

--- rstan/coin_flip_r/coin_flip_ex02/generate.R
+++ rstan/coin_flip_r/coin_flip_ex02/generate.R
@@ -48,3 +48,30 @@ curve(dbeta(x, 1, 1),
 curve(dbeta(x, sum(y) + 1, N - sum(y) + 1),
       from=0, to=1,
       add=TRUE, col='blue', lwd=1.5)
+
+# Fit2 ####
+
+## Generate data: 2nd #####
+
+N <- 10
+theta <- 0.6
+y <- rbinom(N, 1, theta)
+
+data <- list(N=N, y=y) # Named list
+
+## 2nd Fit #####
+
+fit2 <- stan(fit=fit, data=data)
+print(fit2)
+
+theta2 <- extract(fit2, 'theta')
+theta2 <- unlist(theta, use.names=FALSE) 
+head(theta2)
+
+hist(theta2, xlim=c(0,1), freq = FALSE)
+
+# Optimization ####
+
+# https://pystan.readthedocs.io/en/latest/optimizing.html
+
+point_estimate <- optimizing(fit2@stanmodel, data=data)
